d8c31564af480b127f44f93b2f483c8285ed35fb
AleksanderAlex <69598155+AleksanderAlex@users.noreply.github.com>
2024-05-10 18:06:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+
+# Wczytywanie tokenu bota ze zmiennych środowiskowych
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN_SECRET")
+
 # Zmienna intencje przechowuje uprawnienia bota
 intents = discord.Intents.default()
 # Włączanie uprawnienia do czytania wiadomości
 intents.message_content = True
+
+
 # Tworzenie bota w zmiennej klienta i przekazanie mu uprawnień
-client = discord.Client(intents=intents)
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN_SECRET")
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Informacja bota o zalogowaniu sie w terminalu
+@bot.event
 async def on_ready():
-    print(f'Zalogowaliśmy się jako {client.user}')
+    print(f'Zalogowaliśmy się jako {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Cześć!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    else:
-        await message.channel.send(message.content)
+# Komendy bota piszemy w ten sposób
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem {bot.user}')
 
-client.run(TOKEN)
+# A tu przykład komendy, która akceptuje również argument podany przez użytkownika po wpisaniu komendy
+# W tym wypadku $laugh 5 wyświetli nam 'hahahahaha', ponieważ count wtedy przyjmuje wartość 5
+@bot.command()
+async def laugh(ctx, count = 3):
+    await ctx.send("ha" * count)
+
+bot.run(token=TOKEN)
